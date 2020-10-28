8d1deb43cd0257599fc990734a0503588320b52d
Tongxi Lou <tongxi.lou@gmail.com>
2020-10-28 19:43:52 +0100

    add gui

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-import sys
-import argparse
+import tkinter
 from logic_hammer import LogicHammer
 
+root = tkinter.Tk()
+root.title('Logic Hammer')
 
-def main():
-    parser = argparse.ArgumentParser(description='Just stupid')
-    parser.add_argument('--path-in', type=str, help='Input path')
-    parser.add_argument('--path-out', type=str, help='Output path')
+scroll_bar_src = tkinter.Scrollbar(root)
+text_field_src = tkinter.Text(root, height=50, width=120)
 
-    args = parser.parse_args()
+scroll_bar_src.pack(side=tkinter.LEFT, fill=tkinter.Y)
+text_field_src.pack(side=tkinter.LEFT, fill=tkinter.Y)
 
-    with open(args.path_in) as fi:
-        content = fi.read()
+scroll_bar_src.config(command=text_field_src.yview)
+text_field_src.config(yscrollcommand=scroll_bar_src.set)
 
-    hammer = LogicHammer(content)
-    if args.path_out is None:
-        print(hammer)
-    else:
-        open(args.path_out, 'w').write(str(hammer))
+scroll_bar_dst = tkinter.Scrollbar(root)
+text_field_dst = tkinter.Text(root, height=50, width=120)
+
+scroll_bar_dst.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+text_field_dst.pack(side=tkinter.LEFT, fill=tkinter.Y)
+
+scroll_bar_dst.config(command=text_field_dst.yview)
+text_field_dst.config(yscrollcommand=scroll_bar_dst.set)
 
 
-if __name__ == '__main__':
-    main()
+def exec(event):
+    content = text_field_src.get('1.0', tkinter.END).strip()
+    content = content.replace('\n', '')
+    text_field_src.delete('1.0', tkinter.END)
+    text_field_src.insert(tkinter.END, content)
+    result = str(LogicHammer(content))
+    text_field_dst.delete('1.0', tkinter.END)
+    text_field_dst.insert(tkinter.END, result)
+
+
+root.bind('<Return>', exec)
+
+text_field_src.focus()
+tkinter.mainloop()
